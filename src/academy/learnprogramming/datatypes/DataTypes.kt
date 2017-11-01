aa6daa9bc31a7edd8fff7218c7febd3194934510
odys1528 <jolanta.zakrzewska@orange.pl>
2017-11-01 16:50:01 +0100

    arrays

--- src/academy/learnprogramming/datatypes/DataTypes.kt
+++ src/academy/learnprogramming/datatypes/DataTypes.kt
@@ -17,4 +17,20 @@ fun main(args: Array<String>) {
     val vacationTime = false
     val onVacation = DummyClass().isVacationTime(vacationTime)
 
+    //ARRAYS------------------------------------------------------------------------------------------------------------
+    val names = arrayOf("Ada", "Bartek", "Celina", "Donald")
+    val longs1 = arrayOf(1L, 2L, 3L)
+    val longs2 = arrayOf<Long>(1, 2, 3)
+    println(longs1 === longs2)  // false
+
+    val evenNumbers = Array(5){ i -> i * 2 }
+    for (n in evenNumbers) {
+        println(n)
+    }
+
+    val mixedArray = arrayOf(1, "nana", true, null)
+
+    val myIntArray = intArrayOf(1, 2, 3)
+    DummyClass().printNumbers(myIntArray)
+
 }
