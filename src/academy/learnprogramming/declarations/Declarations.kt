eb291ec203168bf5eb22f82212ea573a88efc492
odys1528 <jolanta.zakrzewska@orange.pl>
2017-11-01 14:30:40 +0100

    .equals and ===

--- src/academy/learnprogramming/declarations/Declarations.kt
+++ src/academy/learnprogramming/declarations/Declarations.kt
@@ -25,12 +25,38 @@ fun main(args: Array<String>) {
     val employees: EmployeeSet
 
     val names = arrayListOf("John", "Jane", "Mike")
-    println(names[1])
+    // println(names[1])
+
+
+    val employeeOne = Employee("Mary", 1)
+    val employeeTwo = Employee("John", 2)
+    val employeeThree = Employee("John", 2)
+    println(employeeOne == employeeTwo)  // false
+    println(employeeTwo == employeeThree)  // true (in Java: false)
+    println(employeeOne.equals(employeeTwo))  // false
+    println(employeeTwo.equals(employeeThree))  // true
+    // === like .equals in Java
+    println(employeeOne === employeeTwo)  // false
+    println(employeeTwo === employeeThree)  // false
+
+    val employeeFour = employeeTwo
+    println(employeeFour === employeeTwo)  // true
+    println(employeeFour != employeeTwo)  // false
+    println(employeeFour !== employeeTwo)  // false
+    println(employeeTwo != employeeThree)  // false
+    println(employeeTwo !== employeeThree)  // true
 
 
 }
 
 
 class Employee(var name: String, val id: Int) {
+
+    override fun equals(obj: Any?): Boolean {
+        if (obj is Employee) {
+            return name == obj.name && id == obj.id
+        }
+        return false
+    }
 
 }
