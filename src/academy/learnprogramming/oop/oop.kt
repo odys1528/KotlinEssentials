589c0badc21b40dbc7c77cb3627a86c8fccf5e0a
odys1528 <jolanta.zakrzewska@orange.pl>
2017-11-02 16:49:27 +0100

    classes, constructors

--- src/academy/learnprogramming/oop/oop.kt
+++ src/academy/learnprogramming/oop/oop.kt
@@ -1,14 +1,32 @@
 package academy.learnprogramming.oop
 
-// top-level - default public
+// top-level - public and final by default
 // no matching name requirements
 // private = visible in file
 // internal = visible in module
 
 fun main(args: Array<String>) {
-    val emp = Employee()
+    val emp = Employee("Danuta", "Renata", 123)
 }
 
-private class Employee {
+class Employee constructor(firstName: String, lastName: String, val id: Int) {  // declaration of primary constructor
+    // 'constructor' keyword unnecessary, can be used for defining visibility of constructor
+    // default empty constructor
 
+    val firstName: String  // property declaration
+    val lastName: String = lastName  // 2nd way
+
+    init {  // initializer block, in conjunction with primary constructor
+        this.firstName = firstName
+    }
+
+}
+
+class Employee2(val firstName: String, var id: Int = 666) {  // default parameter
+    var fullTime = true
+
+    constructor(firstName: String, fullTime: Boolean): this(firstName) {  // secondary constructor,
+                                                    // this - passes the value from primary constructor
+        this.fullTime = fullTime
+    }
 }
