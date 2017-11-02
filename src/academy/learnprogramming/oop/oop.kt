7d5f859b9df53059dd40a9eee3a77a7b7fdf8a05
odys1528 <jolanta.zakrzewska@orange.pl>
2017-11-02 17:13:46 +0100

    properties, backing fields

--- src/academy/learnprogramming/oop/oop.kt
+++ src/academy/learnprogramming/oop/oop.kt
@@ -5,9 +5,16 @@ package academy.learnprogramming.oop
 // private = visible in file
 // internal = visible in module
 
+
 fun main(args: Array<String>) {
     val emp = Employee("Danuta", "Renata", 123)
+    // println(emp.firstName)
+    val emp2 = Employee2("Spock")
+    // println(emp2.fullTime)  // error: private
+    val emp3 = Employee3("Kirk")
+    // println(emp3.fullTime)  // running the custom set
 }
+
 
 class Employee constructor(firstName: String, lastName: String, val id: Int) {  // declaration of primary constructor
     // 'constructor' keyword unnecessary, can be used for defining visibility of constructor
@@ -22,11 +29,29 @@ class Employee constructor(firstName: String, lastName: String, val id: Int) {  
 
 }
 
+
 class Employee2(val firstName: String, var id: Int = 666) {  // default parameter
-    var fullTime = true
+    private var fullTime = true
 
     constructor(firstName: String, fullTime: Boolean): this(firstName) {  // secondary constructor,
                                                     // this - passes the value from primary constructor
         this.fullTime = fullTime
+    }
+}
+
+
+// when using getter/setter, variable cannot be declared in primary constructor
+class Employee3(val firstName: String, fullTime: Boolean = true) {
+
+    var fullTime = fullTime
+    // setter/getter directly under declaration
+    get() {
+        println("Running the custom get")
+        return field
+    }
+
+    set(value) {
+        println("Running the custom set")
+        field = value
     }
 }
