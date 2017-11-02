5ac60941236fa24f87cf127e94f9a959e1c347bd
odys1528 <jolanta.zakrzewska@orange.pl>
2017-11-02 16:11:41 +0100

    access modifiers

---
+++ src/academy/learnprogramming/oop/oop.kt
@@ -0,0 +1,14 @@
+package academy.learnprogramming.oop
+
+// top-level - default public
+// no matching name requirements
+// private = visible in file
+// internal = visible in module
+
+fun main(args: Array<String>) {
+    val emp = Employee()
+}
+
+private class Employee {
+
+}
