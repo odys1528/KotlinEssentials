6409479d0fd489304f22ed97fe1af0f0ffbf6fb0
odys1528 <jolanta.zakrzewska@orange.pl>
2017-11-03 11:52:20 +0100

    companion objects

--- src/academy/learnprogramming/oop/Objects.kt
+++ src/academy/learnprogramming/oop/Objects.kt
@@ -4,7 +4,20 @@ import java.time.Year
 
 
 fun main(args: Array<String>) {
+
     println(CompanyCommunications.getCopyrightLine())
+
+    // println(SomeClass.Companion.accessPrivateVar())
+    // println(SomeClass.SomeCompanion.accessPrivateVar())
+    println(SomeClass.accessPrivateVar())
+
+    val someClass1 = SomeClass.justAssign("nana")
+    val someClass2 = SomeClass.upperOrLowerCase("nana", false)
+    println(someClass1.someString)
+    println(someClass2.someString)
+    // val someClass3 = SomeClass()  // error
+
+
 }
 
 
@@ -16,5 +29,40 @@ object CompanyCommunications {
 
     fun getTagLine() = "Our company rocks!"
     fun getCopyrightLine() = "Copyright \u00A9 $currentYear Our Company. All rights reserved."
+
+}
+
+
+class SomeClass private constructor(val someString: String) {
+
+//    val someString: String
+//
+//    constructor(str: String) {
+//        someString = str
+//    }
+//
+//    constructor(str: String, lowerCase: Boolean) {
+//        if (lowerCase) {
+//            someString = str.toLowerCase()
+//        } else {
+//            someString = str.toUpperCase()
+//        }
+//    }
+
+    companion object /*SomeCompanion*/{  // can be called without having an instance, grouping static things
+        private var privateVar = 6
+
+        fun accessPrivateVar() = "privateVar = $privateVar"
+
+        // functions below act like constructors
+        fun justAssign(str: String) = SomeClass(str)
+        fun upperOrLowerCase(str: String, lowerCase: Boolean): SomeClass {
+            if (lowerCase) {
+                return SomeClass(str.toLowerCase())
+            } else {
+                return SomeClass(str.toUpperCase())
+            }
+        }
+    }
 
 }
