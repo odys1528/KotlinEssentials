c930119ac90d3ffe97619ca9017e70b78e79a176
odys1528 <jolanta.zakrzewska@orange.pl>
2017-11-04 13:00:46 +0100

    sets

---
+++ src/academy/learnprogramming/lambda_collections_generics/Sets.kt
@@ -0,0 +1,17 @@
+package academy.learnprogramming.lambda_collections_generics
+
+
+fun main(args: Array<String>) {
+
+    val setInts = setOf(1, 2, 3)  // immutable
+    setInts.plus(20)  // adding an element, doesn't change the set
+    setInts.plus(2)  // no duplicates
+    setInts.minus(2)
+    setInts.average()
+    setInts.drop(2)  // drop first 2 elements
+
+    val mutableInts = mutableSetOf(1, 2, 3, 4, 5)
+    mutableInts.plus(10)  // doesn't change the set
+
+
+}
