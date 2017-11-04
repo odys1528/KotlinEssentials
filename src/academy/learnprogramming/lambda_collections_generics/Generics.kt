15ca6fe01e419ed477dd8c6714a7f5f3e228f9d6
odys1528 <jolanta.zakrzewska@orange.pl>
2017-11-04 16:53:39 +0100

    generics: functions, erasure

--- src/academy/learnprogramming/lambda_collections_generics/Generics.kt
+++ src/academy/learnprogramming/lambda_collections_generics/Generics.kt
@@ -12,10 +12,28 @@ fun main(args: Array<String>) {
 
     list.printCollection2()
 
+    convertToInt(listOf(1f, -2f, 3.14f))
+    append(StringBuilder("string 1"), StringBuilder("string2"))
+
+    val listAny: Any = listOf("nana")
+    println(listAny is List<*>)  // erasure
+
 }
 
 
-fun <T> printCollection(collection: List<T>) {
+fun <T> append(item1: T, item2: T)
+where T: CharSequence, T: Appendable {
+    println(item1.append(item2))
+}
+
+
+fun <T: Number?> convertToInt(collection: List<T>) {
+    collection.forEach { print("${it?.toInt()} ") }
+    println()
+}
+
+
+fun <T: Any> printCollection(collection: List<T>) {  // any = non-nullable
     collection.forEach { print("$it ") }
     println()
 }
